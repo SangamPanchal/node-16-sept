63f05e5c16d02b1ad58deb36f529b613fafa89fd
Your Name <sangampanchal103@gmail.com>
2024-09-17 16:32:06 +0530

    sdf

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -11,16 +11,29 @@ async function addStudent(req, res) {
         console.log(err);
     }
 }
+async function getStudentsByRollNo(req,res) {
+    try {
+        let rollNo = parseInt(req.params.rollNo);
+        console.log(rollNo,'rollNo')
+        let student = await student.findOne({rollNo: rollNo});
+        console.log(student,'students');
+        res.send(student)
+    } catch (err) {
+        console.log(err,'err');
+        
+    }
+}
 async function getStudents(req, res) {
     try {
         let students = await student.find({});
         console.log(students, 'students');
         res.send(students)
     } catch (err) {
-
+        console.log(err,'err');
     }
 }
 module.exports = {
     addStudent,
-    getStudents
+    getStudents,
+    getStudentsByRollNo
 }
