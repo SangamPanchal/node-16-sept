2a545a700ac79a578803b1deb6444abbb5b04046
Your Name <sangampanchal103@gmail.com>
2024-09-17 14:40:52 +0530

    updation...

---
+++ connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+async function connection() {
+    try {
+        let connection = await mongoose.connect('mongodb://localhost:27017/batch-2', { useNewUrlParser: true });
+        console.log("Database Connected");
+
+    } catch (err) {
+        console.log(err.message, 'msg');
+    }
+}
+
+module.exports = connection;
---
+++ controllers/studentController.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const student = require('../models/student');
+async function addStudent(req, res) {
+    try {
+        console.log(req.body, 'req.body')
+        let newStudent = new student(req.body);
+        await newStudent.save();
+        console.log("Data saved successfully");
+        res.end("added....")
+    } catch (err) {
+        console.log(err);
+    }
+}
+async function getStudents(req, res) {
+    try {
+        let students = await student.find({});
+        console.log(students, 'students');
+        res.send(students)
+    } catch (err) {
+
+    }
+}
+module.exports = {
+    addStudent,
+    getStudents
+}
